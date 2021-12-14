468549612e2ffa75317989de08d8deba301a46e0
asmith-personal <asmith3408@gmail.com>
2021-12-14 02:28:22 -0600

    Generalize script
    Generalize script to just assume you want to max by 1-Jan, curr_year+1

--- max_by_eoy.py
+++ max_by_eoy.py
@@ -7,7 +7,7 @@ lvl_99_exp = 13034431
 skills = ["Overall", "Attack", "Defence", "Strength", "Hitpoints", "Ranged", "Prayer", "Magic", "Cooking",
           "Woodcutting", "Fletching", "Fishing", "Firemaking", "Crafting", "Smithing", "Mining", "Herblore",
           "Agility", "Thieving", "Slayer", "Farming", "Runecrafting", "Hunter", "Construction"]
-days_until_eoy = (date(2021, 1, 1) - date.today()).days
+days_until_eoy = (date(date.today().year + 1, 1, 1) - date.today()).days
 
 print(f"Querying OSRS hiscores API for player \'{username}\'... Please wait...")
 response = requests.get(f"https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player={username}")
@@ -24,4 +24,3 @@ for i in range(0,len(skills)):
           f"  Level: {level}\n"
           f"  Exper: {int(exper):,}\n"
           f"Exp/day: {math.trunc((lvl_99_exp - int(exper)) / days_until_eoy):,}")
-
